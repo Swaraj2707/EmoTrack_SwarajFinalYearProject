45cf1a26d50882d0ee12db0e6112d276f3f3f34e
swaraj <singhswaraj521@gmail.com>
2024-06-21 12:29:26 +0530

    update AuthContainer.js

--- src/static/js/components/auth/AuthContainer.js
+++ src/static/js/components/auth/AuthContainer.js
@@ -1,3 +1,29 @@
+// import React from "react";
+// import { GoogleOAuthProvider } from '@react-oauth/google';
+// import Card from '@mui/material/Card';
+// import GoogleAuth from "./GoogleAuth";
+// import LogoColoured from '../../assets/images/emotrack.png';
+// import '../../../css/styles/Auth.scss';
+
+// const AuthContainer = () => {
+//     return (
+//         <>
+//             <div className="googleAuthContainer">
+//                 <Card className="whiteBox authCard">
+//                     <img src={LogoColoured} alt="logo" width="200px" className="logo" />
+//                     <GoogleOAuthProvider clientId="1009385579355-a1ds847gi0od4ppsnu97gj1s1u4vmr6o.apps.googleusercontent.com">
+//                         <GoogleAuth />
+//                     </GoogleOAuthProvider>
+//                 </Card>
+//             </div>
+//             <section className="section-bubble-top"></section>
+//             <section className="section-bubble-bottom"></section>
+//         </>
+//     );
+// }
+
+// export default AuthContainer;
+
 import React from "react";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import Card from '@mui/material/Card';
@@ -23,3 +49,4 @@ const AuthContainer = () => {
 }
 
 export default AuthContainer;
+
